d43e50afe7aa4e6731c0c38842a94b806bba0eb4
cambouvy <45393916+cambouvy@users.noreply.github.com>
2019-05-01 14:26:11 +0200

    Add files via upload

---
+++ svg/Shape.java
@@ -0,0 +1,13 @@
+package svg.shape;
+
+import svg.element.BaseElement;
+
+public abstract class Shape extends BaseElement {
+
+    public Shape(final String label) {
+        super(label);
+    }
+
+
+
+}
