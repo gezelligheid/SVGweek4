934f02b92730ea576315d6b21d39c0fac7d28f6a
cambouvy <45393916+cambouvy@users.noreply.github.com>
2019-05-01 14:42:14 +0200

    Add files via upload

---
+++ svg/shape/Circle.java
@@ -0,0 +1,55 @@
+package svg.shape;
+import svg.SVGParser;
+import svg.element.Element;
+
+
+public class Circle extends Shape {
+
+    private double cx;
+    private double cy;
+    private double r;
+
+    public Circle() {
+        super("circle");
+    }
+
+    public Element newInstance() {
+        return new Circle();
+    }
+
+    public boolean load(final String expr) {
+
+        if (expr.contains(" cx="))
+        {
+            final Double result = SVGParser.extractDouble(expr, " cx=");
+            if (result != null)
+                cx = result.doubleValue();
+        }
+
+        else return false;
+
+        if (expr.contains(" cy="))
+        {
+            final Double result = SVGParser.extractDouble(expr, " cy=");
+            if (result != null)
+                cy = result.doubleValue();
+        }
+
+        else return false;
+
+        if (expr.contains(" r="))
+        {
+            final Double result = SVGParser.extractDouble(expr, " r=");
+            if (result != null)
+                r = result.doubleValue();
+        }
+        else return false;
+
+        return true;
+
+    }
+
+
+
+
+}
---
+++ svg/shape/Ellipse.java
@@ -0,0 +1,65 @@
+package svg.shape;
+import svg.SVGParser;
+import svg.element.Element;
+
+
+public class Ellipse extends Shape {
+
+    private double cx;
+    private double cy;
+    private double rx;
+    private double ry;
+
+    public Ellipse() {
+        super("circle");
+    }
+
+    public Element newInstance() {
+        return new Ellipse();
+    }
+
+    public boolean load(final String expr) {
+
+        if (expr.contains(" cx="))
+        {
+            final Double result = SVGParser.extractDouble(expr, " cx=");
+            if (result != null)
+                cx = result.doubleValue();
+        }
+
+        else return false;
+
+        if (expr.contains(" cy="))
+        {
+            final Double result = SVGParser.extractDouble(expr, " cy=");
+            if (result != null)
+                cy = result.doubleValue();
+        }
+
+        else return false;
+
+        if (expr.contains(" rx="))
+        {
+            final Double result = SVGParser.extractDouble(expr, " rx=");
+            if (result != null)
+                rx = result.doubleValue();
+        }
+        else return false;
+
+        if (expr.contains(" ry="))
+        {
+            final Double result = SVGParser.extractDouble(expr, " ry=");
+            if (result != null)
+                ry = result.doubleValue();
+        }
+        else return false;
+
+
+        return true;
+
+    }
+
+
+
+
+}
