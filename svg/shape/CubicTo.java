20207f75903e1c55140f61a0dab9f78e974ac288
Roman Nekrasov <>
2019-05-01 14:46:45 +0200

    CubicTo

---
+++ svg/shape/CubicTo.java
@@ -0,0 +1,16 @@
+package shape;
+
+public class CubicTo extends PathOp {
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+    double x3;
+    double y3;
+
+    public CubicTo (final String label) {
+        Super(label);
+    }
+
+
+}
